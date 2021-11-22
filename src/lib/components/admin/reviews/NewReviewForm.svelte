24be3d05be1e82c89946cccb29d17b4e29625c7a
Нуфтанчики <300225@mail.ru>
2021-11-22 23:41:36 +0300

    добавил функцию PostNewReview

--- src/lib/components/admin/reviews/NewReviewForm.svelte
+++ src/lib/components/admin/reviews/NewReviewForm.svelte
@@ -10,12 +10,37 @@ import {url_reviews} from '$lib/store/reviews/reviews';
         comment: '',
     };
 
+    async function PostNewReview() {
+      if (new_review.title.length > 1 || new_review.video_link.length > 1 || new_review.img != null || new_review.text.length > 1|| new_review.comment.length > 1) {
+        const response = await fetch($url_reviews, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          body: JSON.stringify(new_review)
+        });
+        let result = await response.json();
+        alert(result.message);
+        
+        console.log( 'значения:', new_review );
+        new_review.title = '';
+        new_review.video_link = '';
+        new_review.img = null;
+        new_review.text = '';
+        new_review.comment = '';
+      } else {
+        console.log('значения не введены')
+      }
+      
+    };
+
+
 </script>
 
-<div class="flex-auto px-4 lg:px-10 py-10 pt-0"> 
-    <form>
+<div class="flex-auto px-4 lg:px-10 py-10 pt-0 bg-green-100"> 
+    <div>
         <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            Новый отзыв
+            Добавить отзыв
         </h6>
         <div class="w-full lg:w-6/12 px-4">
             <div class="relative w-full mb-3">
@@ -25,10 +50,11 @@ import {url_reviews} from '$lib/store/reviews/reviews';
                 Заголовок
               </label>
               <input 
+              bind:value="{ new_review.title }"
               id="title"
               type="text" 
               class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" 
-              value="Отзыв ">
+              >
             </div>
         </div>
 
@@ -40,10 +66,11 @@ import {url_reviews} from '$lib/store/reviews/reviews';
                 Видео
               </label>
               <input 
+              bind:value="{ new_review.video_link }"
               id="video_link"
               type="text" 
               class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" 
-              value="">
+              >
             </div>
         </div>
 
@@ -55,10 +82,11 @@ import {url_reviews} from '$lib/store/reviews/reviews';
                 Фото
               </label>
               <input 
+              bind:value="{ new_review.img }"
               id="img"
               type="file" 
               class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" 
-              value="">
+              >
             </div>
         </div>
 
@@ -71,6 +99,7 @@ import {url_reviews} from '$lib/store/reviews/reviews';
                   Отзыв
                 </label>
                 <textarea 
+                bind:value="{ new_review.text }"
                 id="text"
                 type="text" 
                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"></textarea>
@@ -86,13 +115,17 @@ import {url_reviews} from '$lib/store/reviews/reviews';
                 Коментарий
               </label>
               <input 
+              bind:value="{ new_review.comment }"
               id="comment"
               type="text" 
               class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" 
-              value="">
+              >
             </div>
         </div>
 
-    </form>
+        <button on:click="{PostNewReview}" class="block bg-blue-400 hover:bg-green-600 text-white uppercase text-lg mx-auto p-4 rounded" type="submit">Добавить</button>
+
+    </div>
+
 
 </div>
