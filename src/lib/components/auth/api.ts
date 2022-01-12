24d7c03a37086363447b694f8624ef3d7475d5f0
Нуфтанчики <300225@mail.ru>
2022-01-13 00:01:48 +0300

    установил куки

--- src/lib/components/auth/api.ts
+++ src/lib/components/auth/api.ts
@@ -1,7 +1,9 @@
-const base = 'http://localhost:8000/api/token//';
+// const base = 'http://localhost:8000/api/token/';
+
+const base = 'https://conduit.productionready.io/api';
 
 async function send({ method, path, data, token }) {
-	const opts = { method, headers: {} };
+	const opts:any = { method, headers: {} };
 
 	if (data) {
 		opts.headers['Content-Type'] = 'application/json';
@@ -23,18 +25,18 @@ async function send({ method, path, data, token }) {
 		});
 }
 
-export function get(path, token) {
+export function get(path:any, token:any) {
 	return send({ method: 'GET', path, token });
 }
 
-export function del(path, token) {
-	return send({ method: 'DELETE', path, token });
+export function del(path:any, token:any) {
+	return send ({ method: 'DELETE', path, token });
 }
 
-export function post(path, data, token) {
+export function post(path:any, data:any, token:any) {
 	return send({ method: 'POST', path, data, token });
 }
 
-export function put(path, data, token) {
+export function put(path:any, data:any, token:any) {
 	return send({ method: 'PUT', path, data, token });
 }
