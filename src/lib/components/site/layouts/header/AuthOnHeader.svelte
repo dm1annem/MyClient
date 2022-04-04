2e7b38de1ef301acdb0cbb141681efe51b3346ba
dm1annem <300225@mail.ru>
2022-04-04 20:10:47 +0300

    на кнопке входа

---
+++ src/lib/components/site/layouts/header/AuthOnHeader.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {onMount} from 'svelte'
+    
+
+    let user = {}
+
+    onMount( async() => {
+        const response = await fetch(`/auth/api/profile`);
+        const userData = await response.json();
+        user = userData.user
+    }
+    )
+</script>
+
+{#if user.email}
+    <a href="/auth/profile">
+        {user.email}
+    </a>
+{/if}
+
+<a href="/auth/login/">
+    <i class="fa fa-sign-in  text-xl ml-1 text-gray-500 flex justify-end hover:text-yellow-600"></i>
+
+</a>
