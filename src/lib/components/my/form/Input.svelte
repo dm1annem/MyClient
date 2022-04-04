c8faf63edb911707e087f830e2b67840c3053adc
dm1annem <300225@mail.ru>
2022-04-04 21:22:03 +0300

    начал делать свои компаненты форм

---
+++ src/lib/components/my/form/Input.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    let id = '';
+    let name = 'Имя';
+    let type = 'text';
+    let placeholder ='';
+    let label = '';
+    let class_ = '';
+    
+
+</script>
+
+<div>
+    {#if label}
+        <label for="{id}">{label}</label> 
+    {/if}
+   
+    <input 
+    type="{type}"
+    name="{name}"
+    id="{id}"
+    placeholder={placeholder}
+    class="{class_} border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+    >
+
+</div>
--- src/lib/components/site/home/Form.svelte
+++ src/lib/components/site/home/Form.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Input from '$lib/components/my/form/Input.svelte'
     let name;
     let foneNumber;
 
@@ -10,6 +11,7 @@
             <input type="text"  placeholder="Имя" bind:value={name}>
             <input type="tel" placeholder="Телефон" bind:value={foneNumber}>
             <button>Оставить заявку</button>
+            <Input            />
 
         </form>
     </div>
